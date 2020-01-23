2c0347f616b2355f2644c90febc266e278ce9b53
Frédéric Chapoton <chapoton@unistra.fr>
2020-01-23 13:41:45 +0100

    Jacobi motives, trivial details

--- src/sage/modular/jacobi_motives.py
+++ src/sage/modular/jacobi_motives.py
@@ -20,9 +20,9 @@ REFERENCES:
 
 - http://pmb.univ-fcomte.fr/2018/PMB_Watkins.pdf
 
-- http://magma.maths.usyd.edu.au/magma/handbook/text/1544
+- https://magma.maths.usyd.edu.au/magma/handbook/text/1544
 
-- http://magma.maths.usyd.edu.au/magma/handbook/text/1545
+- https://magma.maths.usyd.edu.au/magma/handbook/text/1545
 
 - https://magma.maths.usyd.edu.au/magma/handbook/text/1546
 
@@ -40,6 +40,7 @@ REFERENCES:
 from sage.rings.rational_field import QQ
 from sage.rings.integer_ring import ZZ
 from sage.arith.functions import lcm
+from sage.arith.misc import euler_phi
 
 
 class JacobiMotive(object):
@@ -144,6 +145,28 @@ class JacobiMotive(object):
         """
         return not self.is_totally_real()
 
+    def degree(self):
+        """
+        Return the degree.
+
+        This is the sum of the Hodge numbers.
+
+        TODO : CHECK : is this correct ?
+        or should we use the degree of the field of definition ?
+
+        .. SEEALSO::
+
+            :meth:`hodge_numbers`
+
+        EXAMPLES::
+
+            sage: from sage.modular.jacobi_motives import JacobiMotive
+            sage: M = JacobiMotive((1/13,3/13,9/13))
+            sage: M.degree()
+            12
+        """
+        return euler_phi(self._m)
+
     def weight(self):
         """
         Return the weight of ``self``.
@@ -164,9 +187,15 @@ class JacobiMotive(object):
         one = ZZ.one()
         return sum(one for _ in self._posi) - sum(one for _ in self._nega)
 
-    def hodge_vector(self):
+    def hodge_numbers(self):
         """
-        Return the Hodge vector of ``self``.
+        Return the Hodge numbers.
+
+        .. SEEALSO::
+
+            :meth:`degree`
+
+        TODO : how can one compute that ?
         """
         m = self._m
         for u in m.coprime_integers(m + 1):
@@ -174,13 +203,17 @@ class JacobiMotive(object):
             local_hodge_weight = ZZ.sum(y - y.floor() for y in iter_posi)
             iter_posi = (u * z for z in self._nega)
             local_hodge_weight -= ZZ.sum(y - y.floor() for y in iter_posi)
-        pass
+        return local_hodge_weight
 
     def effective_weight(self):
         """
         Return the effective weight of ``self``.
 
         This is the width of the Hodge structure.
+
+        .. SEEALSO::
+
+            :meth:`hodge_numbers`
 
         EXAMPLES::
 
