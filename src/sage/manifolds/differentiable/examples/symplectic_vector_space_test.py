c507da7bf9de1cf9903d5f3169b2953981cfa0e4
Tobias Diez <TobiasDiez@gmx.de>
2020-11-27 14:24:10 +0100

    Add documentation to symplectic vector space

--- src/sage/manifolds/differentiable/examples/symplectic_vector_space_test.py
+++ src/sage/manifolds/differentiable/examples/symplectic_vector_space_test.py
@@ -1,3 +1,4 @@
+import sage.all
 from sage.manifolds.differentiable.symplectic_form import SymplecticForm
 from sage.manifolds.differentiable.examples.symplectic_vector_space import SymplecticVectorSpace
 import pytest
@@ -10,8 +11,11 @@ class TestR2VectorSpace:
         return SymplecticVectorSpace(2, 'R2', symplectic_name='omega')
 
     @pytest.fixture
-    def omega(self, M):
+    def omega(self, M: SymplecticVectorSpace):
         return M.symplectic_form()
+
+    def test_repr(self, M: SymplecticVectorSpace):
+        assert M._repr_() == '2-dimensional symplectic vector space R2'
 
     def test_display(self, omega: SymplecticForm):
         assert str(omega.display()) == r'omega = -dq/\dp'
@@ -24,8 +28,11 @@ class TestR4VectorSpace:
         return SymplecticVectorSpace(4, 'R4', symplectic_name='omega')
 
     @pytest.fixture
-    def omega(self, M):
+    def omega(self, M: SymplecticVectorSpace):
         return M.symplectic_form()
+
+    def test_repr(self, M: SymplecticVectorSpace):
+        assert M._repr_() == '4-dimensional symplectic vector space R4'
 
     def test_display(self, omega: SymplecticForm):
         assert str(omega.display()) == r'omega = -dq1/\dp1 - dq2/\dp2'
