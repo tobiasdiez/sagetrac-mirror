741c55abee6e1cc125b16f62d56fc865982c01af
Tobias Diez <code@tobiasdiez.com>
2020-12-22 14:36:41 +0100

    Make autogen work

--- src/sage_setup/autogen/__init__.py
+++ src/sage_setup/autogen/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 
+
 def autogen_all():
     """
     Regenerate the automatically generated files of the Sage library.
@@ -8,8 +9,9 @@ def autogen_all():
     Return a list of sub-packages that should be appended to the list
     of packages built/installed by setup.py.
     """
-
     from . import interpreters
-    interpreters.rebuild(os.path.join("sage", "ext", "interpreters"))
+    from sage.env import SAGE_SRC
+    
+    interpreters.rebuild(os.path.join(SAGE_SRC, "sage", "ext", "interpreters"))
 
     return ['sage.ext.interpreters']
