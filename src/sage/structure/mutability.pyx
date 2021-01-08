df9a54cd7cef639c2875487635706317979b3304
Michael Jung <m.jung@vu.nl>
2021-01-08 11:11:03 +0100

    Trac #31196: use "is_immutable()" + documentation

--- src/sage/structure/mutability.pyx
+++ src/sage/structure/mutability.pyx
@@ -64,7 +64,7 @@ cdef class Mutability:
             sage: v.is_immutable()
             True
         """
-        self._is_immutable
+        return self._is_immutable
 
     cpdef bint is_mutable(self):
         return not self._is_immutable
@@ -76,60 +76,34 @@ def require_mutable(f):
     """
     A decorator that requires mutability for a method to be called.
 
-    EXAMPLES::
+    EXAMPLES:
+
+    To use this decorator, the object requires the method ``is_immutable`` to
+    check for immutability::
 
         sage: from sage.structure.mutability import require_mutable, require_immutable
-        sage: class A(object):
-        ....:     def __init__(self, val):
-        ....:         self._m = val
-        ....:     @require_mutable
-        ....:     def change(self, new_val):
-        ....:         'change self'
-        ....:         self._m = new_val
-        ....:     @require_immutable
-        ....:     def __hash__(self):
-        ....:         'implement hash'
-        ....:         return hash(self._m)
+        sage: class A:
+        ....:  def __init__(self, val):
+        ....:      self._m = val
+        ....:  @require_mutable
+        ....:  def change(self, new_val):
+        ....:      'change self'
+        ....:      self._m = new_val
+        ....:  @require_immutable
+        ....:  def __hash__(self):
+        ....:      'implement hash'
+        ....:      return hash(self._m)
         sage: a = A(5)
-        sage: a.change(6)
         sage: hash(a)
         Traceback (most recent call last):
         ...
-        ValueError: object is mutable; please make it immutable first.
-        sage: a._is_immutable = True
-        sage: hash(a)
-        6
-        sage: a.change(7)   # indirect doctest
-        Traceback (most recent call last):
-        ...
-        ValueError: object is immutable; please use a mutable copy instead.
-        sage: from sage.misc.sageinspect import sage_getdoc
-        sage: print(sage_getdoc(a.change))
-        change self
+        AttributeError: 'A' object has no attribute 'is_immutable'
 
-    AUTHORS:
+    A convenient way to achieve this is to mix in the class
+    :class:`~sage.structure.Mutability`::
 
-    - Simon King <simon.king@uni-jena.de>: initial version
-    - Michael Jung <m.jung@vu.nl>: allow ``_is_mutable`` attribute and new
-      error message
-
-    """
-    @sage_wraps(f)
-    def new_f(self, *args, **kwds):
-        if getattr(self, '_is_immutable', False) or not getattr(self, '_is_mutable', True):
-            raise ValueError("object is immutable; please use a mutable copy instead.")
-        return f(self, *args, **kwds)
-    return new_f
-
-
-def require_immutable(f):
-    """
-    A decorator that requires immutability for a method to be called.
-
-    EXAMPLES::
-
-        sage: from sage.structure.mutability import require_mutable, require_immutable
-        sage: class A(object):
+        sage: from sage.structure.mutability import Mutability, require_mutable, require_immutable
+        sage: class A(Mutability):
         ....:  def __init__(self, val):
         ....:      self._m = val
         ....:  @require_mutable
@@ -146,7 +120,7 @@ def require_immutable(f):
         Traceback (most recent call last):
         ...
         ValueError: object is mutable; please make it immutable first.
-        sage: a._is_immutable = True
+        sage: a.set_immutable()
         sage: hash(a)
         6
         sage: a.change(7)
@@ -160,13 +134,85 @@ def require_immutable(f):
     AUTHORS:
 
     - Simon King <simon.king@uni-jena.de>: initial version
-    - Michael Jung <m.jung@vu.nl>: allow ``_is_mutable`` attribute and new
-      error message
+    - Michael Jung <m.jung@vu.nl>: mutability check via method
 
     """
     @sage_wraps(f)
     def new_f(self, *args, **kwds):
-        if not getattr(self, '_is_immutable', False) and getattr(self, '_is_mutable', True):
+        if self.is_immutable():
+            raise ValueError("object is immutable; please use a mutable copy instead.")
+        return f(self, *args, **kwds)
+    return new_f
+
+
+def require_immutable(f):
+    """
+    A decorator that requires immutability for a method to be called.
+
+    EXAMPLES:
+
+    To use this decorator, the object requires the method ``is_immutable`` to
+    check for immutability::
+
+        sage: from sage.structure.mutability import require_mutable, require_immutable
+        sage: class A:
+        ....:  def __init__(self, val):
+        ....:      self._m = val
+        ....:  @require_mutable
+        ....:  def change(self, new_val):
+        ....:      'change self'
+        ....:      self._m = new_val
+        ....:  @require_immutable
+        ....:  def __hash__(self):
+        ....:      'implement hash'
+        ....:      return hash(self._m)
+        sage: a = A(5)
+        sage: hash(a)
+        Traceback (most recent call last):
+        ...
+        AttributeError: 'A' object has no attribute 'is_immutable'
+
+    A convenient way to achieve this is to mix in the class
+    :class:`~sage.structure.Mutability`::
+
+        sage: from sage.structure.mutability import Mutability, require_mutable, require_immutable
+        sage: class A(Mutability):
+        ....:  def __init__(self, val):
+        ....:      self._m = val
+        ....:  @require_mutable
+        ....:  def change(self, new_val):
+        ....:      'change self'
+        ....:      self._m = new_val
+        ....:  @require_immutable
+        ....:  def __hash__(self):
+        ....:      'implement hash'
+        ....:      return hash(self._m)
+        sage: a = A(5)
+        sage: a.change(6)
+        sage: hash(a)   # indirect doctest
+        Traceback (most recent call last):
+        ...
+        ValueError: object is mutable; please make it immutable first.
+        sage: a.set_immutable()
+        sage: hash(a)
+        6
+        sage: a.change(7)
+        Traceback (most recent call last):
+        ...
+        ValueError: object is immutable; please use a mutable copy instead.
+        sage: from sage.misc.sageinspect import sage_getdoc
+        sage: print(sage_getdoc(a.__hash__))
+        implement hash
+
+    AUTHORS:
+
+    - Simon King <simon.king@uni-jena.de>: initial version
+    - Michael Jung <m.jung@vu.nl>: mutability check via method
+
+    """
+    @sage_wraps(f)
+    def new_f(self, *args, **kwds):
+        if not self.is_immutable():
             raise ValueError("object is mutable; please make it immutable first.")
         return f(self, *args, **kwds)
     return new_f
